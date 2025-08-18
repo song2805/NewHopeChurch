952afe8b1e0f37739bbd56a0f2d31763414ab88a
Je <song2805@empas.com>
2025-08-18 11:48:44 -0700

    ver 0.0.3

--- script.js
+++ script.js
@@ -44,3 +44,12 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const nav = document.querySelector('header nav');
+
+    hamburger.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    });
+});
